23709c71a3614020137bb3ff55bb41f6317d8114
John Neal <johnneal@MacBook-Pro-269.local>
2025-07-19 17:58:49 -0500

    added grab indicator color changes and also player redirect bullet function

--- bullet.py
+++ bullet.py
@@ -1,17 +1,25 @@
 import pygame
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, pos, direction, speed=5):
+    def __init__(self, pos, direction, speed=5, grabbable=False, owner='enemy'):
         super().__init__()
         self.image = pygame.Surface((10, 10), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, (0, 0, 255), (5, 5), 5)
+        
+        # Color by owner
+        color = (0, 0, 255) if owner == 'enemy' else (0, 255, 0)
+        pygame.draw.circle(self.image, color, (5, 5), 5)
+        
         self.rect = self.image.get_rect(center=pos)
-
         self.pos = pygame.Vector2(pos)
         self.velocity = direction.normalize() * speed
-    
+        self.grabbable = grabbable
+        self.owner = owner  # 'enemy' or 'player'
+
     def update(self):
         self.pos += self.velocity
         self.rect.center = self.pos
-        if self.pos.x < 0 or self.pos.x > 800 or self.pos.y > 800 or self.pos.y < 0:
+
+        # Kill if off screen (assuming 800x800 window)
+        if (self.pos.x < 0 or self.pos.x > 800 or
+            self.pos.y < 0 or self.pos.y > 800):
             self.kill()
