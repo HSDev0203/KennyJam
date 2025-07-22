af0f14a6f99d17a72d3dfa86c0929ebe9422a867
John Neal <johnneal@MacBook-Pro-269.local>
2025-07-22 18:29:17 -0500

    slightly changed start dialogue and font

--- dialogue.py
+++ dialogue.py
@@ -4,13 +4,14 @@ class DialogueManager:
     def __init__(self, screen):
         
         self.screen = screen
-        self.introDiologues = ["Welcome to the game of REDIRECTION! (press space to continue)", 
-                               "In this game, you use your enemy's power against them...",
-                               "press E to grab an enemy's bullet within your grab radius...",
-                               "then, draw the best circle you can to REDIRECT",
+        self.introDiologues = ["Welcome to Re-Direct! (press space to continue)", 
+                                "In this game, you use your enemy's POWER against them...",
+                                "When an enemy's BULLET is within your GRAB RADIUS,",
+                                "Left click or press E."
+                                "Then, draw a circle with your cursor to REDIRECT",
                                 "their attack back at them!",
-                                "also you can press shift to dodge",
-                               "Have fun and good luck! (press space to start)"]
+                                "Use WASD to move and press shift to DODGE",
+                                "Have fun and good luck! (press space to start)"]
         self.runningDialogues = []
 
 
@@ -19,7 +20,7 @@ class DialogueManager:
         self.BLACK = (0, 0, 0)
 
         # Font
-        self.font = pygame.font.Font("Assets/fonts/P8.ttf", 20)
+        self.font = pygame.font.Font("Assets/fonts/EightBitDragon.ttf", 18)
 
         self.current_line = 0
 
@@ -27,6 +28,8 @@ class DialogueManager:
         self.dialogue_box = pygame.Rect(0, 600, 800, 200)
 
         self.running = True
+
+
     def update(self, skip_key):
         if self.running:
             if skip_key:
@@ -37,9 +40,9 @@ class DialogueManager:
     def draw(self):
         if self.current_line < len(self.runningDialogues):
 
-            pygame.draw.rect(self.screen, self.BLACK, self.dialogue_box)
-            pygame.draw.rect(self.screen, self.WHITE, self.dialogue_box.inflate(-10, -10))
+            pygame.draw.rect(self.screen, self.WHITE, self.dialogue_box)
+            pygame.draw.rect(self.screen, self.BLACK, self.dialogue_box.inflate(-4, -4))
 
-            text_surface = self.font.render(self.runningDialogues[self.current_line], True, self.BLACK)
+            text_surface = self.font.render(self.runningDialogues[self.current_line], False, self.WHITE)
             self.screen.blit(text_surface, (self.dialogue_box.x + 20, self.dialogue_box.y + 40))
 
