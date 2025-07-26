d8f99e82818408a57aae244cf6f462f0d1676534
HSLegacy <henryseefeldt@proton.me>
2025-07-26 10:40:26 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/HSDev0203/KennyJam""
    This reverts commit 43b052e69335e7f696931c5b8cfae81c1c49ead0.

--- soundeffects.py
+++ soundeffects.py
@@ -2,10 +2,19 @@ import pygame
 
 pygame.mixer.init()
 
-attack_sounds = [pygame.mixer.Sound('Assets/sfx/shoot.wav'), pygame.mixer.Sound('Assets/sfx/shoot_2.wav'), pygame.mixer.Sound('Assets/sfx/shoot_3.wav')]
+attack_sounds = [pygame.mixer.Sound('Assets/sfx/shoot_1.wav'), 
+                 pygame.mixer.Sound('Assets/sfx/shoot_2.wav')]
+attack_sounds[0].set_volume(1.2)
+attack_sounds[1].set_volume(0.5)
+
 player_shoot_sound = pygame.mixer.Sound('Assets/sfx/player_shoot.wav')
-enemy_hurt_sound = pygame.mixer.Sound('Assets/sfx/hitHurt.wav')
-text_skip_sound = pygame.mixer.Sound('Assets/sfx/text.wav')
+
+enemy_hurt_sound = pygame.mixer.Sound('Assets/sfx/enemy_hurt.wav')
+
+text_skip_sound = pygame.mixer.Sound('Assets/sfx/text_skip.wav')
+text_skip_sound.set_volume(0.45)
+
 player_hurt_sound = pygame.mixer.Sound('Assets/sfx/player_hurt.wav')
-dash_sound = pygame.mixer.Sound('Assets/sfx/whoosh.wav')
-game_loss_sound = pygame.mixer.Sound('Assets/sfx/wilhelm_scream.wav')
+
+dash_sound = pygame.mixer.Sound('Assets/sfx/dash.wav')
+dash_sound.set_volume(0.5)
